fcda5479d78422ac037fc09aefd40f0a83a847e8
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-07 15:04:54 +0600

    gameobject function created with home and away as parameters

---
+++ src/00-objectball.js
@@ -0,0 +1,17 @@
+// build the gameobject function
+
+function gameObject() {
+  return {
+    home: {
+      teamName: "",
+      colors: "",
+      players: ""
+    },
+    away: {
+      teamName: "",
+      colors: "",
+      players: ""
+    },
+    },
+
+    }
